e3d464e1e16ca932ad6c96ab619864da28d34be3
MaterialFuture <konstantinev@protonmail.com>
2021-10-19 16:36:53 -0500

    Updated core and books

--- src/clj_bible_lib/books.clj
+++ src/clj_bible_lib/books.clj
@@ -1,19 +1,11 @@
 (ns clj-bible-lib.books
   (:require [clojure.java.io :as io]
-            [clj-bible-lib.core :as cr]
-            [clojure.string :as str]))
+            [clj-bible-lib.core :as cr]))
 
 (defn update-books-list []
   "Parse data file and gather information on the books within"
-  ;(if (not (cr/resource-data? "books")))
-  (dedupe (for [data (str/split-lines (slurp (io/resource cr/data-bible)))]
-            (first (str/split data #"\t")))))
-;;TODO: Going to redo this by formatting the data as a vector and passing it to this function
+  ;(if (not (cr/resource-data? "books"))) ;TODO: Make sure checks work
+  (vec (dedupe (for [data (cr/data->formatted-vector cr/data-bible-name)]
+                 (first data)))))
 
 (def books-list (memoize update-books-list))              ;cache books list
-
-(defn save-books-to-file []
-  (with-open [p (clojure.java.io/writer cr/data-bible-books-loc)]
-    (doseq [line books-list]
-      (.write p line))))
-;;TODO: This may not be needed since the list is cached once BOOKS-LIST is referenced
