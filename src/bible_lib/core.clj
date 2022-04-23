1d948edae84ee7c95f3b7a268bb33a93fd1318cf
MaterialFuture <konstantinev@protonmail.com>
2022-04-23 17:20:30 -0500

    Update core with books

--- src/bible_lib/core.clj
+++ src/bible_lib/core.clj
@@ -5,8 +5,6 @@
 (def data-loc "resources/")                                 ;can also use io/resource for this
 (def data-bible-name "kjv.tsv")                             ;name for data file
 (def data-bible-loc (str data-loc data-bible-name))         ;used for .exists
-;(def data-bible-books "books.txt")                          ;converted data for easier parsing
-;(def data-bible-books-loc (str data-loc data-bible-books))  ;converted data for easier parsing
 
 (defn write-file [data file]
   (with-open [w (io/writer file)]
@@ -24,7 +22,7 @@
     (str/split data #"\t"))))
 
 ;Memoize bible data to speed up all other actions requiring this data
-(def bible-data (memoize data->formatted-vector)) ;ex. (first (data-bible)) => < .1 msec
+(def bible-data (memoize data->formatted-vector))           ;ex. (first (data-bible)) => < .1 msec
 
 (defn format-verse-number [verse]
   "Takes verse vector and formats it to XX:XX"
@@ -36,13 +34,20 @@
 
 (defn string-data [data]
   "Turn bible data into a collection of strings - punctuation"
-  (str/join " "
-            (for [d data]
-              (remove-punctuation (last d)))))              ;TODO - Change for to map or other func
+  (str/join " " (for [d data]
+                  (remove-punctuation (last d)))))          ;TODO - Change for to map or other func
 
 (defn frequency-data [data]
   "Create lazyseq displaying ([WORD, COUNT]...)"
   ;TODO - Map data to vector with labels
-  (sort-by last
-           (frequencies (str/split (string-data data)
-                                   #" "))))
+  ;TODO - Change Case for all words
+  (sort-by last (frequencies (str/split (string-data data) #" ")))) ;Print to file using write-file
+
+
+
+(defn update-books-list []
+  "Parse data file and gather information on the books within"
+  (vec (dedupe (for [data (cr/bible-data)]
+                 (first data)))))
+
+(def data-books-list (memoize update-books-list))           ;cache books list
